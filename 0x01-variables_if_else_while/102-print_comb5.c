bc1e980031684606977e56552fba7edef96f8ed5
winniewandia <winniewandia63@gmail.com>
2023-03-20 07:51:31 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints combinations of two two numbered values
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 1; l < 10; l++)
+				{
+					if (j < l && i <= k)
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(' ');
+						putchar('0' + k);
+						putchar('0' + l);
+						if (i == 9 && j == 8)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
