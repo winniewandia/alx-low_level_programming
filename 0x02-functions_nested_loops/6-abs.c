40c9df0767cf0f3652d51b8217ce536b135a6b04
winniewandia <winniewandia63@gmail.com>
2023-03-21 09:07:07 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Prints absolute value of a number
+ * @num: the value to be changed to absolute
+ *
+ * Return: Absolute value
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		num = -1 * num;
+		return (num);
+	}
+	else
+	{
+		return (num);
+	}
+}
+
