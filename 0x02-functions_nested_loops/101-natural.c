55194cc9535c4e5a1107dbf6bac8e3576171672e
winniewandia <winniewandia63@gmail.com>
2023-03-22 10:00:55 -0700

    print natural numbers and their sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * natural_numbers - prints all natural numbers below 1024
+ * that are multiples of 3 and 5 and adds them
+ */
+
+void natural_numbers(void)
+{
+	int n, sum;
+
+	n = 0;
+	sum = 0;
+	while (n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+		n++;
+	}
+	printf("%d", sum);
+	putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,3 +18,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void print_long(long var);
+void natural_numbers(void);
