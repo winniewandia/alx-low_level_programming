562ed08899d6013b345d55e25a7cb7f1cda201ae
winniewandia <winniewandia63@gmail.com>
2023-03-22 23:07:30 -0700

    Check for capital letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _isupper - Used to check if a character is in upper case
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is in uppercase
+ * and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' || c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char C;
+
+	C = 'A';
+	_isupper(C);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+/**
+ * contains the prototypes of all my functions
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void print_long(long var);
+void natural_numbers(void);
+void fibonacci_numbers(void);
+void even_fibonacci(void);
+int _isupper(int c);
