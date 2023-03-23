091755369f5b5e896e018e3fee175478781dcf9d
winniewandia <winniewandia63@gmail.com>
2023-03-23 08:11:57 -0700

    draw diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,6 +18,7 @@ void print_diagonal(int n)
 			if (i == j)
 			{
 				_putchar('\\');
+				break;
 			}
 			else
 			{
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * fizz_buzz - prints numbers 0 -100
+ * if no is divisible by 3, prints fizz
+ * if divisible by 5, prints buzz
+ * if divisible by both, prints fizzbuzz
+ */
+
+void fizz_buzz(void)
+{
