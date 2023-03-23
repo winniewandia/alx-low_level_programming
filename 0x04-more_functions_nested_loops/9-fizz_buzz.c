49e8cfd6289cdd4624f004e090cfc1727fd1f41f
winniewandia <winniewandia63@gmail.com>
2023-03-23 08:58:46 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * fizz_buzz - prints numbers 0 -100
+ * fizz_buzz - prints numbers 1 - 100
  * if no is divisible by 3, prints fizz
  * if divisible by 5, prints buzz
  * if divisible by both, prints fizzbuzz
@@ -10,3 +10,41 @@
 
 void fizz_buzz(void)
 {
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			if (i != 100)
+			{
+				putchar(' ');
+			}
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
