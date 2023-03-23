b38380979653ded7754c416f9f4f42fb59b603c7
winniewandia <winniewandia63@gmail.com>
2023-03-23 05:32:28 -0700

    draw line on terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Prints straight line on terminal
+ * @n: Number of times _ should be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (i <= n)
+	{
+		if (n <= 0)
+			break;
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
