37580ad5401112584c4cf7dce47f560e7b2eb6cd
winniewandia <winniewandia63@gmail.com>
2023-03-27 23:28:59 -0700

    display n no of integers in array with eof check

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		if (a[i] != '\0')
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			return;
+		}
 	}
 	_putchar('\n');
 }
