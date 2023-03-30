3483585d795a37f6b972d173fc115404fc5274a4
winniewandia <winniewandia63@gmail.com>
2023-03-30 07:17:52 -0700

    Capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+
+/*
+ * is_alpha - Check if character is a letter
+ * @c: Character to be checked
+ *
+ * Return: Returned character
+ */
+
+int is_alpha(char c)
+{
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+/**
+ * cap_string - Capitalizes each letter ar the begining of a word
+ * @str: String to be capitalized
+ *
+ * Return: Capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int i, new_word;
+
+	i = 0;
+	new_word = 1;
+	while (str[i] != '\0')
+	{
+		if (new_word && is_alpha(str[i]))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= ('a' - 'A');
+			}
+			new_word = 0;
+		}
+		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '\"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
+		{
+			new_word = 1;
+		}
+		i++;
+	}
+	return str;
+}
+
