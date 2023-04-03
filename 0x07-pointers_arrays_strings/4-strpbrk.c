9cf6edc672fb92d22e018b79d46919ca2c48137f
winniewandia <winniewandia63@gmail.com>
2023-04-03 05:18:30 -0700

    _strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - Searches a string for the first occurence
+ * of any character from a specified set of characters
+ * @s: Pointer to string that is to be searched
+ * @accept: Pointer to string that contains set of characters to be searched
+ *
+ * Return: Pointer to first occurrence of any character
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+
+	while (*s != '\0')
+	{
+		p = accept;
+		while (*p != '\0')
+		{
+			if (*s == *p)
+			{
+				return (s);
+			}
+			p++;
+		}
+		s++;
+	}
+	return (NULL);
+}
+
