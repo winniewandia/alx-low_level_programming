c529ea3992a20592892267e99fc97cd059b54fc3
winniewandia <winniewandia63@gmail.com>
2023-04-11 04:52:13 -0700

    print sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Print sum of positive numbers
@@ -12,24 +13,28 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, sum, j;
 
 	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) > 0 && argc > 1)
+		char *arg = argv[i];
+		j = 0;
+		while (arg[j] != '\0')
 		{
-			sum += atoi(argv[i]);
+			if (!isdigit(arg[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
 		}
-		else if (argc == 1)
-		{
-			printf("0\n");
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		sum += atoi(arg);
 	}
 	printf("%d\n", sum);
 	return (0);
