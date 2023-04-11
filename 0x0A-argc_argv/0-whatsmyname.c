f92479ec24b625e65ad9c2ae299ae1041fe50596
winniewandia <winniewandia63@gmail.com>
2023-04-11 03:05:13 -0700

    Display program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Used tp print a program's name
+ * @argc: Number of arguments
+ * @argv: Array of pointers to arguments
+ *
+ * Return: Always 0 success
+ */
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
