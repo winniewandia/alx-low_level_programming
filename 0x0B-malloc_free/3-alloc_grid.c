05b2c35098a79ceced10a49b1eacf0ce61adf2b3
winniewandia <winniewandia63@gmail.com>
2023-04-11 10:59:41 -0700

    2D grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	arr = malloc(sizeof(int) * height);
+	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
