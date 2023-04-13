56e88dea6cb7475c69447ed078a6ea70c5a9978c
winniewandia <winniewandia63@gmail.com>
2023-04-13 05:17:50 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,12 +13,19 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+	unsigned int i;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+	p = (char *)arr;
+	for (i = 0; i < nmemb * size; i++)
+	{
+		p[i] = 0;
+	}
 	return (arr);
 }
 
