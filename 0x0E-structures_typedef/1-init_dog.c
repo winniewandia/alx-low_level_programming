40ef243528234d4b749444fcc8768e6233afc173
winniewandia <winniewandia63@gmail.com>
2023-04-17 03:14:00 -0700

    Allocated memory for name and owner

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * init_dog - Initializes a variable of type struct dog
@@ -12,11 +13,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return;
-	d->name = name;
+	d->name = malloc(strlen(name) + 1);
+	strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = malloc(strlen(owner) + 1);
+	strcpy(d->owner, owner);
 }
 
