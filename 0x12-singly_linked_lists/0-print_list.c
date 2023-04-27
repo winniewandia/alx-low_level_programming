6080f944f79ebfedf48b95e58a5762f8c7e963ad
winniewandia <winniewandia63@gmail.com>
2023-04-27 07:48:09 -0700

    added new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,12 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[%d] %s\n", 0, "nil");
 		}
 		else
 		{
-			printf("[%d] (%s)", h->len, h->str);
+			printf("[%d] (%s)\n", h->len, h->str);
 		}
-		printf("\n");
 		h = h->next;
 		n++;
 	}
