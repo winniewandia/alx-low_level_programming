73af7c82407efd9d2a0fa6eeeb0806411f065e0d
winniewandia <winniewandia63@gmail.com>
2023-04-27 10:05:55 -0700

    frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list
+ * @head: Pointer to the head of list
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *next, *current;
+
+	current = head;
+	while (next != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
+
