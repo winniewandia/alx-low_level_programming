1e40840c8f03b5acd5e78430b6d7e255c4d22732
winniewandia <winniewandia63@gmail.com>
2023-04-27 08:20:37 -0700

    Returns number of elements in a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,7 +20,9 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
 
