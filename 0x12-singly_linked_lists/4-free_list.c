0eadcf3d2a0492a49dd6ce2f827a324fd82f29e2
winniewandia <winniewandia63@gmail.com>
2023-05-02 02:24:05 -0700

    A function that frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list
+ * @head: Pointer to head of list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
+
