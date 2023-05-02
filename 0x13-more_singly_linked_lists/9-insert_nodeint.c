ff7e3731d7045ad4aa76ff8cfb27a3d8c8ce2942
winniewandia <winniewandia63@gmail.com>
2023-05-02 07:02:01 -0700

    Changed to new->next

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	previous->next = new;
-	current = current->next;
+	new->next = current;
 	return (new);
 }
 
