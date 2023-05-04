14a6ff0100c0d6b77a9f400f00c0a1ed9ad9d46f
winniewandia <winniewandia63@gmail.com>
2023-05-04 05:02:01 -0700

    decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,69 @@
+#include "main.h"
+
+/**
+ * _power - returns the power of a base number
+ * @base: The base number
+ * @pow: The number the base is raised to
+ *
+ * Return: Base raised to power
+ */
+
+unsigned long int _powerl(unsigned long int base, unsigned long int pow)
+{
+	unsigned long int result, i;
+
+	result = 1;
+	for (i = 0; i < pow; i++)
+	{
+		result *= base;
+	}
+	return (result);
+}
+
+/**
+ * print_binary - converts decimal to binary
+ * @n: Decimal to be converted
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i, max, diff, j, result, k;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		for (max = 0; _powerl(2, max) <= n; max++)
+			;
+		i = max - 1;
+		if (_powerl(2, i) == n)
+		{
+			_putchar('1');
+			for (k = 0; k < max - 1; k++)
+			{
+				_putchar('0');
+			}
+		}
+		else
+		{
+			result = n;
+			for (j = i; j <= i; j--)
+			{
+				diff = result;
+				if (diff >= _powerl(2, j))
+				{
+					diff -= _powerl(2, j);
+					_putchar('1');
+				}
+				else
+				{
+					_putchar('0');
+				}
+				result = diff;
+			}
+		}
+	}
+}
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 #include <stddef.h>
+#include <unistd.h>
 
+int _putchar(char c);
 unsigned int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
+unsigned long int _powerl(unsigned long int base, unsigned long int pow);
+void print_binary(unsigned long int n);
 
 #endif
