070387b29c8925f90efac33456b6478f18bdc15e
winniewandia <winniewandia63@gmail.com>
2023-06-15 13:02:11 +0300

    free list betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,13 +6,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *current, *next;
+	dlistint_t *current, *next;
 
-    current = head;
-    while (current != NULL)
-    {
-        next = current->next;
-        free(current);
-        current = next;
-    }
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
 }
