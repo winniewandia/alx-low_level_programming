921316b6eb0f0135fe0894d313e83712c6a08f69
winniewandia <winniewandia63@gmail.com>
2023-06-15 02:31:24 -0700

    betty for 1st task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,18 +8,18 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count;
+	size_t count;
 
-    count = 0;
-    if (h == NULL)
-        return (count);
-    while (h->prev != NULL)
-        h = h->prev;
-    while (h != NULL)
-    {
-        count += 1;
-        printf("%d\n", h->n);
-        h = h->next;
-    }
-    return (count);
+	count = 0;
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		count += 1;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
 }
