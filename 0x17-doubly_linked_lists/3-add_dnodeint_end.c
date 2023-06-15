7bd202de68c93093336e356ea3f958ca31533f72
winniewandia <winniewandia63@gmail.com>
2023-06-15 13:00:47 +0300

    add node end betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,29 +9,29 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new, *last;
+	dlistint_t *new, *last;
 
-    if (head == NULL)
-        return (NULL);
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = n;
-    new->next = NULL;
-    if (*head == NULL)
-    {
-        new->prev = NULL;
-        *head = new;
-    }
-    else
-    {
-        last = *head;
-        while (last->next != NULL)
-        {
-            last = last->next;
-        }
-        new->prev = last;
-        last->next = new;
-    }
-    return (new);
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		new->prev = last;
+		last->next = new;
+	}
+	return (new);
 }
