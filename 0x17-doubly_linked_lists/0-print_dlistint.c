92b9251b99f77c5110cfb59650b5d26052bc1d82
winniewandia <winniewandia63@gmail.com>
2023-06-15 12:23:14 +0300

    added h->prev loop

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,6 +11,10 @@ size_t print_dlistint(const dlistint_t *h)
     size_t count;
 
     count = 0;
+    if (h == NULL)
+        return (count);
+    while (h->prev != NULL)
+        h = h->prev;
     while (h != NULL)
     {
         count += 1;
