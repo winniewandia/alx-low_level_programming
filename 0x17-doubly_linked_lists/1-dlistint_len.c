7b86b6da96c048d669c6b313d27b85ae9a6b3ced
winniewandia <winniewandia63@gmail.com>
2023-06-15 12:48:01 +0300

    bett 2

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count;
+	size_t count;
 
-    count = 0;
-    while (h != NULL)
-    {
-        count += 1;
-        h = h->next;
-    }
-    return (count);
+	count = 0;
+	while (h != NULL)
+	{
+		count += 1;
+		h = h->next;
+	}
+	return (count);
 }
