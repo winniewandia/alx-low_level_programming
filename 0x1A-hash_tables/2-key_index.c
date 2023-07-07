c6d93d5ca1c71cb33c3c2357e383b46b293808fd
winniewandia <winniewandia63@gmail.com>
2023-07-07 12:26:47 +0300

    betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * Key_index - function that gives you the index of a key
+ * key_index - function that gives you the index of a key
  * @key: The key
  * @size: The size of the array of the hash table
  *
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,24 +6,24 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *temp;
-    unsigned long int i;
-    char *delim = "";
+	hash_node_t *temp;
+	unsigned long int i;
+	char *delim = "";
 
-    if (ht == NULL)
-    {
-        return;
-    }
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        temp = ht->array[i];
-        while (temp != NULL)
-        {
-            printf("%s'%s': '%s'", delim, temp->key, temp->value);
-            delim = ", ";
-            temp = temp->next;
-        }
-    }
-    printf("}\n");
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			printf("%s'%s': '%s'", delim, temp->key, temp->value);
+			delim = ", ";
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
 }
