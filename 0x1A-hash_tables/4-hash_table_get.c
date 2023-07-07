4d10b703e466c544edfffd9eaa0fbcb13ac5c7e6
winniewandia <winniewandia63@gmail.com>
2023-07-07 12:04:43 +0300

    added the loop

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -19,12 +19,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	item = ht->array[index];
-	if (item != NULL)
+	while (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
 		{
 			return (item->value);
 		}
+		item = item->next;
 	}
 	return (NULL);
 }
